3a018860703834a9d5f3273694b67b149953dbf7
Muhammed Eren Sönmez <erensonmez.bussines@gmail.com>
2024-04-09 21:14:54 +0300

    Add files via upload

---
+++ 4. Scope/app.js
@@ -0,0 +1,34 @@
+// var degiskenIsmi = 10;
+
+// console.log(degiskenIsmi)
+
+// var a = 5; // global scope
+// sen buna her yerden elisebilirsin.
+
+// if(true){
+        
+// }
+// function method1(){
+//     console.log(a)
+// }
+// method1
+
+// function method1(){  // Function Scope
+//     var sayi= 10;
+//     console.log(sayi)    
+// }
+// method1();
+
+function method1(){
+    var a=5; // Function scope
+    if(true){
+        var a=5; // Bloke scope
+    }
+    while(true){
+
+    }
+    for(let i = 0 ; i<=10; i++){
+
+    }
+}
+
