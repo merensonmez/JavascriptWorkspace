fd23cff88a1d5deee945f5e78752fd46a12aee1a
Muhammed Eren Sönmez <erensonmez.bussines@gmail.com>
2024-04-09 21:15:22 +0300

    Add files via upload

---
+++ 6. Data Types/app.js
@@ -0,0 +1,77 @@
+/*
+
+? Data Types
+
+1-String
+2-Number
+3-Boolean
+4-Null
+5-Undefined
+6-Object
+7-Function
+*/
+
+//------- STRING DATATYPE------//
+// let name = "Eren Sonmez 19 years old.";
+// console.log(name);
+// console.log(typeof name);
+
+// let number1 = 10.7;
+// let number2 = 5.3;
+
+// console.log(number1+number2);
+// console.log(typeof number1);
+ 
+
+//------- NUMBER DATATYPE------//
+// let a = "5";
+// let b = "2";
+
+// Your Age is 19;
+
+// let stringVariable = "Your age : ";
+// let age = 19;
+
+// console.log(stringVariable + age)\
+
+//------- BOOLEAN DATATYPE------//
+//boolean : creates from true and false.
+
+// console.log(5>8);
+
+// let a = 3;
+// let b = 12;
+
+// let result = a+b;
+// console.log(result!=15) 
+
+//------- NULL DATATYPE------//
+// let a = null; // empty
+// a = 27;
+// console.log(a);
+
+//------- UNDEFINED DATATYPE------//
+
+// let a;
+// console.log(typeof a)
+
+// what is primitive and referance types , what is the difference?
+//------- OBJECT DATATYPE------//
+// let human = {
+//     name : "Eren",
+//     surname : "Sonmez",
+//     age : 23
+// }
+// console.log(human)
+// console.log(typeof human)
+
+
+// let numbers = [1,2,3,4,5];
+// console.log(typeof numbers);
+
+let func = function(){
+    console.log("Hello");
+}
+
+func();
+console.log(typeof func)
