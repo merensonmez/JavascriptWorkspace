e69b3618e99f21dd2d8225da31e640533aba22a1
Muhammed Eren Sönmez <erensonmez.bussines@gmail.com>
2024-04-10 01:24:16 +0300

    Add files via upload

---
+++ 10.Conditionals Using Else If/app.js
@@ -0,0 +1,17 @@
+// Else If
+
+
+let selectedPath = prompt("Please,you chose want to go path :");
+
+if(selectedPath ==1){
+    alert("Selected path " + selectedPath+". is path.");
+}
+else if(selectedPath ==2){
+    alert("Selected path " + selectedPath+". is path.");
+}
+else if(chosenPath ==3){
+    alert("Selected path " + selectedPath+". is path.");
+}
+else{
+    alert("Please choose invalid path !!");
+}
