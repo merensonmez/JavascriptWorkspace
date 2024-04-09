fcd713ef3651e4d514affa82438ef33ac5a4f951
Muhammed Eren Sönmez <erensonmez.bussines@gmail.com>
2024-04-09 21:14:24 +0300

    Add files via upload

---
+++ 3. Cikti verme/app.js
@@ -0,0 +1,50 @@
+//Document write ile cikti
+
+//document.write("Hello World <br>");
+//document.write("Eren Sonmez");
+//document.write(55)
+
+//console.log("Hello World");
+//console.log("Eren Sonmez");
+//console.log(3.53);
+//console.log(true);
+//console.log({name:"Eres",surname:"Sonmez"});
+
+
+//let a=5;
+//let b=1221;
+
+//console.log("Two number addition : "+ a+b);
+//console.clear()
+//console.log(window);
+
+//let a = 10;
+//let b = 12;
+
+//alert("Two number addition : " + (a+b))
+//alert("Not click F5 keyboard.");
+
+//console.error("Hata olustu");
+//console.warn("Hata olustu");
+//debugger;
+//console.log(window.location.port);
+//console.log(window.location.port);
+//console.log(window.location.port);
+//console.log(window.location.port);
+
+
+// COMMENT LINE --- Don't forget to read here. 
+// SAWD
+// SADASD
+// COMMENT LINE
+// SADASD
+
+
+/*
+
+   BETTER COMMENTS
+   ? Information line
+   ! Important information
+   TODO : Should to this
+    *Test Message
+*/   
