df9189ea01c2555f03deebb4f3e05285b5665bdb
Muhammed Eren Sönmez <erensonmez.bussines@gmail.com>
2024-04-10 01:23:50 +0300

    Add files via upload

---
+++ 8.Dialog Boxes/app.js
@@ -0,0 +1,28 @@
+/* 
+         -----Dialog Boxes-----
+         - Alert
+         - Prompt
+         - Confirm
+
+*/
+console.log(window)
+
+
+// Function
+// alert("Before you continue file, you need to save .")
+
+
+// Prompt();
+// let name = prompt("What's your name?");
+// let age = prompt("Your age?");
+
+// console.log("Name : " +name)
+// console.log("Age : " +age)
+
+// console.log(typeof name)
+// console.log(typeof age)
+
+
+// Confirm
+// let result =confirm ("Are you sure to delete file?");
+// console.log(result);
